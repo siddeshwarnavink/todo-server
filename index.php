f4f2f8ea5968bd855cbc3ad08d8fab5d0da8bc80
Siddeshwar <jjsiddeshwar61@gmail.com>
2019-04-21 19:09:15 +0530

    Modifyed the CORS

--- index.php
+++ index.php
@@ -52,8 +52,11 @@ try {
     $status = 500;
 }
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-Type: application/json', true, $status);
 
 echo json_encode($output);
